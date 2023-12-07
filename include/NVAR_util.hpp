5e5cf691f039c92657abe9ce70dad87b5e509506
Ben Andrews <benafandrews@gmail.com>
2023-12-07 23:25:51 +0000

    Begun specific compile time implementation branch.

--- include/NVAR_util.hpp
+++ include/NVAR_util.hpp
@@ -34,34 +34,30 @@ using Index = std::ptrdiff_t;
 template <typename T>
 concept Weight = std::floating_point<T> || is_complex<T>::value;
 
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using Vec = Eigen::Vector<T, N>;
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using RowVec = Eigen::RowVector<T, N>;
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using RefVec = Eigen::Ref<Vec<T, N>>;
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using RefRowVec = Eigen::Ref<RowVec<T, N>>;
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using ConstRefVec = Eigen::Ref<const Vec<T, N>>;
-template <Weight T, Index N = Eigen::Dynamic>
+template <Weight T, Index N>
 using ConstRefRowVec = Eigen::Ref<const RowVec<T, N>>;
-template <Index N = Eigen::Dynamic>
-using Indices = Eigen::Vector<Index, N>;
-template <Index N = Eigen::Dynamic>
-using RefIndices = Eigen::Ref<Indices<N>>;
-template <Weight T, Index R = Eigen::Dynamic, Index C = Eigen::Dynamic>
+template <Weight T, Index R, Index C>
 using Mat = Eigen::Matrix<T, R, C>;
-template <Weight T, Index R = Eigen::Dynamic, Index C = Eigen::Dynamic>
+template <Weight T, Index R, Index C>
 using RefMat = Eigen::Ref<Mat<T, R, C>>;
-template <Weight T, Index R = Eigen::Dynamic, Index C = Eigen::Dynamic>
+template <Weight T, Index R, Index C>
 using ConstRefMat = Eigen::Ref<const Mat<T, R, C>>;
 
 using FeatureVecShape = std::vector<std::tuple<NVAR::Index, NVAR::Index>>;
-template <Weight T, Index R = -1, Index C = -1>
+template <Weight T, Index R, Index C>
 using DataPair = std::tuple<Mat<T, R, C>, Mat<T, R, C>>;
 
-template <typename T, NVAR::Index R = -1, NVAR::Index C = -1>
+template <typename T, NVAR::Index R, Index C>
 std::string
 mat_shape_str( const NVAR::ConstRefMat<T, R, C> m ) {
     return std::format( "({}, {})", m.rows(), m.cols() );
@@ -84,22 +80,6 @@ factorial_20() {
     return result;
 }
 
-constexpr inline Index
-factorial_20( const Index N ) {
-    assert( N >= 0 && N < 21 );
-    if ( N >= 21 ) {
-        std::cerr << std::format( "Attempted factorial_20({}).", N )
-                  << std::endl;
-        exit( EXIT_FAILURE );
-    }
-    if ( N == 0 ) {
-        return Index{ 1 };
-    }
-    Index val{ 1 };
-    for ( Index i{ 1 }; i <= N; ++i ) { val *= i; }
-    return val;
-}
-
 template <Index d, Index k, Index p, nonlinear_t Nonlinearity>
 consteval inline Index
 def_nonlinear_size() {
@@ -120,21 +100,6 @@ def_nonlinear_size() {
     }
 }
 
-template <nonlinear_t Nonlinearity>
-constexpr inline Index
-def_nonlinear_size( const Index d, const Index k, const Index p ) {
-    if constexpr ( Nonlinearity == nonlinear_t::exp ) {
-        return d * k;
-    }
-    else {
-        if ( p == 0 ) {
-            return Index{ 0 };
-        }
-        assert( d > 0 && k > 0 && p > 0 );
-        return factorial_20( d * k + p - 1 )
-               / ( factorial_20( p ) * factorial_20( d * k - 1 ) );
-    }
-}
 
 template <Index d, Index k, Index p, nonlinear_t Nonlinearity,
           bool constant = true>
@@ -151,71 +116,21 @@ def_total_size() {
     }
 }
 
-template <nonlinear_t Nonlinearity>
-constexpr inline Index
-def_total_size( const Index d, const Index k, const Index p,
-                const bool constant = true ) {
-    return def_nonlinear_size<Nonlinearity>( d, k, p ) + d * k
-           + ( constant ? 1 : 0 );
-}
-
-template <Weight T, Index d, Index k, Index p>
-constexpr inline Vec<T, def_nonlinear_size<d, k, p, nonlinear_t::poly>()>
-combinations_with_replacement( const ConstRefVec<T, d * k> v ) {
-    if constexpr ( p == 0 ) {
-        return Vec<T, 0>{};
-    }
-
-    constexpr Index result_sz{
-        def_nonlinear_size<d, k, p, nonlinear_t::poly>()
-    };
-    constexpr Index n{ d * k };
-    Index           count{ 0 };
-
-    std::vector<Index> indices( p, 0 );
-    Vec<T, result_sz>  result{ Vec<T, result_sz>::Ones() };
-
-    while ( true ) {
-        // Add current result
-        for ( const auto i : indices ) { result[count] *= v[i]; }
-        count++;
-
-        // Find rightmost index to increment
-        auto j = p - 1;
-        while ( j >= 0 && indices[static_cast<std::size_t>( j )] == n - 1 ) {
-            j--;
-        }
-
-        // If no index found, break out
-        if ( j < 0 ) {
-            break;
-        }
-
-        // Increment found index & adjust subsequent
-        indices[static_cast<std::size_t>( j )]++;
-        for ( Index i{ j + 1 }; i < p; ++i ) {
-            indices[static_cast<std::size_t>( i )] =
-                indices[static_cast<std::size_t>( i - 1 )];
-        }
-    }
-
-    return result;
-}
-
-template <Weight T>
-constexpr inline Vec<T>
-combinations_with_replacement( const ConstRefVec<T> v, const Index d,
-                               const Index k, const Index p ) {
+template <Weight T, Index d, Index k, Index p, Index N>
+constexpr inline Vec<T, N>
+combinations_with_replacement( const ConstRefVec<T, N> v ) {
     if ( p == 0 ) {
-        return Vec<T>{};
+        return Vec<T, 0>{};
     }
 
     const Index n{ d * k };
     Index       count{ 0 };
 
     std::vector<Index> indices( static_cast<std::size_t>( p ), 0 );
-    Vec<T>             result{ Vec<T>::Ones(
-        def_nonlinear_size<nonlinear_t::poly>( d, k, p ) ) };
+
+    auto result{
+        Vec<T, def_nonlinear_size<d, k, p, nonlinear_t::poly>>::Ones()
+    };
 
     while ( true ) {
         // Add current result
@@ -245,9 +160,8 @@ combinations_with_replacement( const ConstRefVec<T> v, const Index d,
 }
 
 // Implementation from: https://github.com/pconstr/eigen-ridge.git
-template <Weight T>
-constexpr inline Mat<T>
-ridge( const Mat<T> A, const Mat<T> y, const T alpha ) {
+constexpr inline Mat<T, R, C>
+ridge( const Mat<T, R, C> A, const Mat<T, R, C> y, const T alpha ) {
     const auto & svd = A.jacobiSvd( Eigen::ComputeFullU | Eigen::ComputeFullV );
     const auto & s = svd.singularValues();
     const auto   r = s.rows();
