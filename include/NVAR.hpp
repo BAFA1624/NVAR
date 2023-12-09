4785e1c6108b8d64adec0f5a1824e9606e718d2a
Ben Andrews <benafandrews@gmail.com>
2023-12-09 00:39:34 +0000

    Reconstruction & target_diff added as NVAR params.
    	- Added training data reconstruction as constructor params for
    	  NVAR class.
    	- Added target_diff as a templated boolean option to alter
    	  whether the NVAR learns to predict the next value, or the
    	  difference between the current & next value.
    	- Cleaned up code with some better comments & removed any
    	  std::cout statements left behind.

--- include/NVAR.hpp
+++ include/NVAR.hpp
@@ -8,14 +8,11 @@
 #include <format>
 #include <vector>
 
-// #define TARGET_DIFFERENCE
-// #define ALT_RIDGE
-// #define WRITE_FEATURES 10
-
 namespace NVAR
 {
 
-template <Weight T, nonlinear_t Nonlin = nonlinear_t::poly>
+template <Weight T, nonlinear_t Nonlin = nonlinear_t::poly,
+          bool target_difference = false>
 class NVAR
 {
     private:
@@ -29,11 +26,20 @@ class NVAR
     T      m_ridge_param;
     Mat<T> m_w_out;
 
-    //
-    Index m_n_training_inst;
-    Index m_n_linear_feat;
-    Index m_n_nonlinear_feat;
-    Index m_n_total_feat;
+    // Data sizes
+    Index m_n_training_inst;  // Number of training samples
+    Index m_n_linear_feat;    // Size of linear feature vector
+    Index m_n_nonlinear_feat; // Size of nonlinear feature vector
+    Index m_n_total_feat;     // Size of total feature vector
+
+    // Other internal variables
+    // When true, attempts to reconstruct training data from the training total
+    // feature vector & the learned weights
+    bool m_reconstruct_training;
+    // Column titles for output file when m_reconstruct_training = true
+    std::vector<std::string> m_col_titles;
+    // File to write reconstructed data to
+    std::filesystem::path m_reconstruction_path;
 
     [[nodiscard]] constexpr inline Mat<T>
     construct_linear_vec( const ConstRefMat<T> samples ) const noexcept;
@@ -55,7 +61,9 @@ class NVAR
     NVAR( const ConstRefMat<T> samples, const ConstRefMat<T> labels,
           const Index d, const Index k, const Index s, const Index p,
           const T ridge_param, const bool use_constant,
-          const T constant = T{ 0. } ) :
+          const T constant = T{ 0. }, const bool reconstruct_training = false,
+          const std::vector<std::string> & reconstruction_titles = {},
+          const std::filesystem::path      reconstruction_path = "" ) :
         m_d( d ),
         m_k( k ),
         m_s( s ),
@@ -66,7 +74,9 @@ class NVAR
         m_n_training_inst( samples.rows() - s * ( k - 1 ) ),
         m_n_linear_feat( d * k ),
         m_n_nonlinear_feat( def_nonlinear_size<Nonlin>( d, k, p ) ),
-        m_n_total_feat( def_total_size<Nonlin>( d, k, p, use_constant ) ) {
+        m_n_total_feat( def_total_size<Nonlin>( d, k, p, use_constant ) ),
+        m_reconstruct_training( reconstruct_training ),
+        m_reconstruction_path( reconstruction_path ) {
         if ( labels.rows() != m_n_training_inst ) {
             std::cerr << std::format(
                 "The number of training labels ({}) must equal the number of "
@@ -75,77 +85,66 @@ class NVAR
                 labels.rows(), m_n_training_inst, m_d, m_k, m_s );
             exit( 1 );
         }
-        // std::cout << "Constructing linear features.\n";
         const Mat<T> linear_features{ construct_linear_vec( samples ) };
-        // std::cout << linear_features.leftCols( 10 ) << "\n";
-        //  std::cout << "Constructing nonlinear features.\n";
         const Mat<T> nonlinear_features{ construct_nonlinear_vec(
             linear_features ) };
-        // std::cout << nonlinear_features.leftCols( 10 ) << "\n";
-        //  std::cout << "Constructing total features.\n";
         const Mat<T> total_features{ construct_total_vec(
             linear_features, nonlinear_features ) };
 
-        // std::cout << std::format( "m_n_training_inst: {}, total_features:
-        // {}\n",
-        //                           m_n_training_inst,
-        //                           mat_shape_str<T, -1, -1>( total_features )
-        //                           );
-        //  std::cout << total_features.leftCols( 10 ) << "\n";
-        //  std::cout << "Performing ridge regression.\n";
-#ifdef TARGET_DIFFERENCE
-        m_w_out = ridge_regress( labels - samples, total_features );
-#else
-        m_w_out = ridge_regress( labels, total_features );
-#endif
-        // std::cout << "m_w_out:\n" << m_w_out.transpose() << std::endl;
-#ifndef HH_MODEL
-    #ifdef FORECAST
-        const std::filesystem::path    path{ "../data/forecast_data/tmp.csv" };
-        const std::vector<std::string> col_titles{ "I", "V", "I'", "V'" };
-    #endif
-    #ifdef CUSTOM_FEATURES
-        const std::filesystem::path    path{ "../data/forecast_data/tmp.csv" };
-        const std::vector<std::string> col_titles{ "V_(n)",    "V_(n-1)",
-                                                   "I_(n)",    "V_(n)'",
-                                                   "V_(n-1)'", "I_(n)'" };
-    #endif
-    #ifdef DOUBLESCROLL
-        const std::filesystem::path path{
-            "../data/forecast_data/doublescroll_reconstruct.csv"
-        };
-        const std::vector<std::string> col_titles{ "v1", "v2", "I" };
-    #endif
-    #ifdef HH_MODEL
-        const std::filesystem::path    path{ "../data/forecast_data/tmp.csv" };
-        const std::vector<std::string> col_titles{ "Vmembrane", "Istim" };
-    #endif
-        // std::cout << std::format(
-        //     "m_w_out: ({}, {}), total_features: ({}, {})\n", m_w_out.rows(),
-        //     m_w_out.cols(), total_features.rows(), total_features.cols() );
-    #ifndef TARGET_DIFFERENCE
-        Mat<T> reproduced = ( m_w_out * total_features ).transpose();
-    #else
-        Mat<T> reproduced_differences =
-            ( m_w_out * total_features ).transpose();
-        Mat<T> reproduced = samples.bottomRows( reproduced_differences.rows() );
-        std::cout << std::format( "samples: ({}, {})\n", samples.rows(),
-                                  samples.cols() );
-        std::cout << std::format( "reproduced_differences: ({}, {})\n",
-                                  reproduced_differences.rows(),
-                                  reproduced_differences.cols() );
-        for ( Index c{ 0 }; c < reproduced_differences.rows(); ++c ) {
-            reproduced.row( c ) += reproduced_differences.row( c );
+        if constexpr ( target_difference ) {
+            m_w_out = ridge_regress( labels - samples, total_features );
         }
-    #endif
-        // std::cout << std::format( "reproduced: ({}, {})\n",
-        // reproduced.rows(),
-        //                           reproduced.cols() );
+        else {
+            m_w_out = ridge_regress( labels, total_features );
+        }
 
-        Mat<T> reconstructed( reproduced.rows(), reproduced.cols() * 2 );
-        reconstructed << reproduced, labels;
-        SimpleCSV::write<T>( path, reconstructed, col_titles );
-#endif
+        if ( m_reconstruct_training ) {
+            // Set column titles to use when writing results out
+            if ( static_cast<Index>( reconstruction_titles.size() ) == m_d ) {
+                m_col_titles = reconstruction_titles;
+
+                // Add titles for the labels
+                for ( const auto & title : reconstruction_titles ) {
+                    std::cout << title << "'" << std::endl;
+                    m_col_titles.push_back( title + "'" );
+                }
+            }
+            else {
+                // Titles for samples
+                for ( Index i{ 0 }; i < m_d; ++i ) {
+                    m_col_titles.push_back( std::to_string( i ) );
+                }
+                // Titles for labels
+                for ( Index i{ 0 }; i < m_d; ++i ) {
+                    m_col_titles.push_back( std::to_string( i ) + "'" );
+                }
+            }
+
+            // Attempt to regenerate training data
+            auto regenerated{ ( m_w_out * total_features ).transpose() };
+            if constexpr ( target_difference ) {
+                for ( Index r{ 0 }; r < samples.rows(); ++r ) {
+                    regenerated.row( r ) += samples.row( r );
+                }
+            }
+
+            // Add labels for plotting
+            Mat<T> reconstruction( regenerated.rows(),
+                                   regenerated.cols() + labels.cols() );
+            reconstruction << regenerated, labels;
+
+            // Write to file
+            if ( std::filesystem::directory_entry( m_reconstruction_path )
+                     .exists() ) {
+                SimpleCSV::write<T>( m_reconstruction_path, reconstruction,
+                                     m_col_titles );
+            }
+            else {
+                std::cerr << std::format(
+                    "Unable to write reconstruction data to path: {}\n",
+                    m_reconstruction_path.string() );
+            }
+        }
     }
 
     [[nodiscard]] constexpr inline auto w_out() const noexcept {
@@ -157,9 +156,9 @@ class NVAR
               const std::vector<Index> & pass_through ) const noexcept;
 };
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::construct_linear_vec(
+NVAR<T, Nonlin, target_difference>::construct_linear_vec(
     const ConstRefMat<T> samples ) const noexcept {
     Mat<T> linear_features( m_n_linear_feat, m_n_training_inst );
     for ( Index i{ 0 }; i < m_n_training_inst; ++i ) {
@@ -168,9 +167,9 @@ NVAR<T, Nonlin>::construct_linear_vec(
     return linear_features;
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::construct_nonlinear_vec(
+NVAR<T, Nonlin, target_difference>::construct_nonlinear_vec(
     const ConstRefMat<T> linear_features ) const noexcept {
     Mat<T> nonlinear_features{ Mat<T>::Zero( m_n_nonlinear_feat,
                                              m_n_training_inst ) };
@@ -187,9 +186,9 @@ NVAR<T, Nonlin>::construct_nonlinear_vec(
     return nonlinear_features;
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Vec<T>
-NVAR<T, Nonlin>::construct_nonlinear_inst(
+NVAR<T, Nonlin, target_difference>::construct_nonlinear_inst(
     const ConstRefVec<T> linear_feature ) const noexcept {
     Vec<T> nonlinear_feature{ Vec<T>::Zero( m_n_nonlinear_feat ) };
     if constexpr ( Nonlin == nonlinear_t::exp ) {
@@ -203,9 +202,9 @@ NVAR<T, Nonlin>::construct_nonlinear_inst(
     return nonlinear_feature;
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::construct_total_vec(
+NVAR<T, Nonlin, target_difference>::construct_total_vec(
     const ConstRefMat<T> linear_features,
     const ConstRefMat<T> nonlinear_features ) const noexcept {
     Mat<T> total_features{ Mat<T>::Zero( m_n_total_feat, m_n_training_inst ) };
@@ -224,54 +223,34 @@ NVAR<T, Nonlin>::construct_total_vec(
     return total_features;
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::construct_total_vec(
+NVAR<T, Nonlin, target_difference>::construct_total_vec(
     const ConstRefMat<T> samples ) const noexcept {
     const auto linear_features{ construct_linear_vec( samples ) };
     const auto nonlinear_features{ construct_nonlinear_vec( linear_features ) };
     return construct_total_vec( linear_features, nonlinear_features );
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::ridge_regress(
+NVAR<T, Nonlin, target_difference>::ridge_regress(
     const ConstRefMat<T> labels,
     const ConstRefMat<T> total_feature_vec ) noexcept {
     const auto feature_vec_product{ total_feature_vec
                                     * total_feature_vec.transpose() };
-    // std::cout << "feature vec det: " << feature_vec_product.determinant()
-    //           << std::endl;
     const auto tikhonov_matrix{
         m_ridge_param * Mat<T>::Identity( m_n_total_feat, m_n_total_feat )
     };
-    // std::cout << "tikhonov_matrix det: " << tikhonov_matrix.determinant()
-    //           << std::endl;
-
-    // std::cout << std::format( "feature_vec: ({}, {})\n",
-    //                           feature_vec_product.rows(),
-    //                           feature_vec_product.cols() );
-    // std::cout << std::format( "tikhonov_matrix: ({}, {})\n",
-    //                           tikhonov_matrix.rows(), tikhonov_matrix.cols()
-    //                           );
-
     // L2 regularization adapts linear regression to ill-posed problems
     const auto sum{ feature_vec_product + tikhonov_matrix };
     const auto factor{ sum.completeOrthogonalDecomposition().pseudoInverse() };
-
-    // std::cout << std::format( "labels: {}, factor: {}, total_feature_vec:
-    // {}\n",
-    //                           mat_shape_str<T, -1, -1>( labels ),
-    //                           mat_shape_str<T, -1, -1>( factor ),
-    //                           mat_shape_str<T, -1, -1>( total_feature_vec )
-    //                           );
-
     return labels.transpose() * ( factor * total_feature_vec ).transpose();
 }
 
-template <Weight T, nonlinear_t Nonlin>
+template <Weight T, nonlinear_t Nonlin, bool target_difference>
 [[nodiscard]] constexpr inline Mat<T>
-NVAR<T, Nonlin>::forecast(
+NVAR<T, Nonlin, target_difference>::forecast(
     const ConstRefMat<T> warmup, const ConstRefMat<T> labels,
     const std::vector<Index> & pass_through ) const noexcept {
     [[maybe_unused]] const Index max_idx{ *std::max_element(
@@ -283,53 +262,39 @@ NVAR<T, Nonlin>::forecast(
     const Index N{ labels.rows() };
     Mat<T>      result{ Mat<T>::Zero( N, m_d ) };
     RowVec<T>   new_val( 2 );
-#ifdef TARGET_DIFFERENCE
-    Mat<T> prev_value{ warmup.row( warmup.rows() - 1 ) };
-#endif
-    Mat<T> samples = warmup;
-#ifdef WRITE_FEATURES
-    Mat<T> forecast_features( m_n_total_feat, N );
-#endif
+    RowVec<T>   prev_value{ warmup.row( warmup.rows() - 1 ) };
+    Mat<T>      samples = warmup;
 
     for ( Index i{ 0 }; i < N; ++i ) {
+        // Construct total feature vector
         Vec<T> lin_feat = construct_x_i<T>( samples, m_k, m_s );
-
         Vec<T> nonlin_feat = construct_nonlinear_inst( lin_feat );
-
         Vec<T> total_feat{ Vec<T>::Zero(
             def_total_size<Nonlin>( m_d, m_k, m_p, m_use_constant ) ) };
-
         if ( m_use_constant ) {
             total_feat << lin_feat, nonlin_feat, m_c;
         }
         else {
             total_feat << lin_feat, nonlin_feat;
         }
-#ifdef WRITE_FEATURES
-        forecast_features.col( i ) = total_feat;
-#endif
-#ifdef TARGET_DIFFERENCE
-        new_val = prev_value + ( m_w_out * total_feat ).transpose();
-#else
+
+        // Make next step prediction
         new_val = ( m_w_out * total_feat ).transpose();
-#endif
+        if constexpr ( target_difference ) {
+            new_val += prev_value;
+        }
+
+        // Pass-through any specified true values
         for ( const auto idx : pass_through ) {
             new_val[idx] = labels( i, idx );
         }
 
+        // Assign prediction to result & cycle samples
         result.row( i ) << new_val;
         samples = cycle_inputs<T>( samples, result.row( i ) );
-#ifdef TARGET_DIFFERENCE
         prev_value << result.row( i );
-#endif
     }
-#ifdef WRITE_FEATURES
-    const std::filesystem::path feature_path = {
-        "../data/forecast_data/features1.csv"
-    };
-    SimpleCSV::write<T>( feature_path,
-                         forecast_features.leftCols( WRITE_FEATURES ), {} );
-#endif
+
     return result;
 }
 
