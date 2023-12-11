ca94544e8d279e570434372047ce727a81d2b349
Ben Andrews <benafandrews@gmail.com>
2023-12-11 16:26:03 +0000

    Beginning to build out ESN structure.

--- include/ESN/ESN.hpp
+++ include/ESN/ESN.hpp
@@ -9,13 +9,24 @@ template <Weight T>
 class ESN
 {
     private:
+    Index m_n_node;
+    T     m_leak;
+    T     m_spectral_radius;
+
     Vec<T> m_reservoir;
     Mat<T> m_w_in;
     Mat<T> m_w_out;
     Mat<T> m_adjacency;
 
+    [[nodiscard]] constexpr inline Vec<T>
+    R_next( const ConstRefVec<T> R ) const noexcept;
+    [[nodiscard]] constexpr inline Mat<T>
+    train_W_out( const ConstRefMat<T> outputs,
+                 const ConstRefMat<T> labels ) const noexcept;
+
     public:
-    ESN() {}
+    ESN( const ConstRefVec<T> features, const ConstRefVec<T> labels,
+         const T leak ) {}
 
     // Getters
     [[nodiscard]] constexpr inline ConstRefVec<T> R() const noexcept {
@@ -30,6 +41,10 @@ class ESN
     [[nodiscard]] constexpr inline ConstRefMat<T> A() const noexcept {
         return m_adjacency;
     }
+
+    [[nodiscard]] constexpr inline Mat<T>
+    forecast( const ConstRefVec<T> warmup, const ConstRefVec<T> labels,
+              const std::vector<Index> & pass_through = {} ) const noexcept;
 };
 
 } // namespace ESN
